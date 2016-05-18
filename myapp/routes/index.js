5e8caa9bf50801e73313dc316a98f599501716ee
Çağatay ÇALI <me@calicagatay.me>
2016-05-18 21:53:48 +0300

    Basic conversation

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,12 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/wup', function(req, res, next) {
-  res.render('index', {title: 'What\'s up bro'});
-});
-
-router.get('/thx', function (req, res) {
-  res.render('index', {title: 'Thx dude.'});
-});
-
 module.exports = router;
